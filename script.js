4b13e41dd88ff57fa3fb40e5d331b3a9b4a011a8
Robertmuxa2 <alimzhanbukali@gmail.com>
2024-10-06 20:16:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ gl.viewport(0, 0, canvas.width, canvas.height);
 // Configurable parameters
 const config = {
 	particleCount: 5000,
-	textArray: ["tE.", "Ate.", "SYNDICATE."],
+	textArray: ["WE", "ARE", "SYNDICATE."],
 	mouseRadius: 0.1,
 	particleSize: 2,
 	forceMultiplier: 0.001,
