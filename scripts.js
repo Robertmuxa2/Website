58158ed6a95068729c903a322f9201cf944cf72b
Robertmuxa2 <alimzhanbukali@gmail.com>
2024-10-06 14:08:02 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,15 @@
-// Анимация заголовка и картинки
 document.getElementById('start-btn').addEventListener('click', function() {
-  // Сдвиг заголовка
   const title = document.getElementById('game-title');
   const image = document.getElementById('game-image');
+  const imageContainer = document.getElementById('image-container');
   
   title.classList.add('moved-title');
   
-  // Появление картинки
   setTimeout(() => {
+    imageContainer.style.display = 'block';
     image.classList.add('reveal-image');
   }, 500);
 
-  // Переадресация на другой сайт через 3 секунды
   setTimeout(() => {
     window.location.href = 'https://2no.co/15bgk7.gif';
   }, 3000);
